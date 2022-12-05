42da939bfd6f62649601005d3500cd703adb6f37
x-farkasle <x-farkasle@wizzair.com>
2022-12-05 15:46:14 +0100

    starter effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import './App.css';
 import { FiAperture } from 'react-icons/fi';
+import { BiSun, BiMoon } from 'react-icons/bi';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [theme, setDarkMode] = useState('light');
+
+  const toggleDarkMode = () => {
+    if (theme === 'light') {
+      setDarkMode('dark');
+    } else {
+      setDarkMode('light');
+    }
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
     <div className="App">
       <main>
         <div className="wrapper">
-          <span>Now you see me</span>
+          <span className="sub-title">
+            {theme === 'light' ? 'Now you see me' : 'Now you dont'}
+          </span>
           <h1>Light</h1>
           <div className="triangle" />
           <a href="#">
-            Get Started <FiAperture className="icon" />
+            Get Started <FiAperture className="started-icon" />
           </a>
+          <button class="theme" onClick={() => toggleDarkMode()}>
+            {theme === 'dark' ? (
+              <span>
+                Dark mode <BiMoon className="theme-icon" />
+              </span>
+            ) : (
+              <span>
+                Light mode <BiSun className="theme-icon" />
+              </span>
+            )}
+          </button>
         </div>
       </main>
     </div>
